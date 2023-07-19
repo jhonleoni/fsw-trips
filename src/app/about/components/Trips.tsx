05d09e73aac103ba90b11509be5393f203dd284c
jhonleoni <jhonyalmeidax@gmail.com>
2023-07-18 20:57:24 -0400

    feat: update database schema

---
+++ src/app/about/components/Trips.tsx
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma"
+import React, { useState } from "react"
+
+const getTrips = async () => {
+    const trips = await prisma.trip.findMany({})
+
+    return trips
+}
+
+const Trips = async () => {
+  const data = await fetch("http:jsonplaceholder.typicode.com/posts", {
+      next:{
+        revalidate: 0  
+      }
+  }).then(res => res.json())
+
+  console.log({ data })
+
+  return <div>{data.map((i: any) => (<p key={i.id}>{i.title}</p>))}</div>
+}
+
+export default Trips
