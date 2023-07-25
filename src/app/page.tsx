da4058a0a0849b8365799841c877c41cf1a39d8c
jhonleoni <jhonyalmeidax@gmail.com>
2023-07-24 22:25:46 -0400

    feat: add recommended trips component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import QuickSearch from "./components/QuickSearch"
 import TripSearch from "./components/TripSearch"
+import RecommendedTrips from "./components/RecommendedTrips"
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
       <QuickSearch />
+      <RecommendedTrips />
     </div>
   )
 }
