45b198167da6a5dbc86cb18328d7c9c8f639b788
jhonleoni <jhonyalmeidax@gmail.com>
2023-08-01 19:51:46 -0400

    feat: add trip reservation component

--- src/app/trips/[tripId]/components/TripReservation.tsx
+++ src/app/trips/[tripId]/components/TripReservation.tsx
@@ -1,14 +1,30 @@
+"use client"
+
+import Button from '@/components/Button'
 import DatePicker from '@/components/DatePicker'
+import Input from '@/components/Input'
+import { Trip } from '@prisma/client'
 import React from 'react'
 
-const TripReservation = () => {
-   return (
-     <div className='flex flex-col'>
-       <div className="flex">
-        <DatePicker placeholderText='Data de início' onChange={() => {}}/>
-        <DatePicker placeholderText='Data final' onChange={() => {}}/>
+interface TripReservationProps {
+  trip: Trip
+}
 
+const TripReservation = ({ trip }: TripReservationProps) => {
+   return (
+     <div className='flex flex-col px-5'>
+       <div className="flex gap-4">
+        <DatePicker placeholderText='Data de início' onChange={() => {}} className='w-full'/>
+        <DatePicker placeholderText='Data final' onChange={() => {}} className='w-full'/>
        </div>
+
+       <Input placeholder= {`Número de hóspedes (máx: ${trip.maxGuests})`} className='mt-4'/>
+
+       <div className="flex justify-between mt-3">
+        <p className='font-medium text-sm text-primaryDarker'>Total: </p>
+        <p className='font-medium text-sm text-primaryDarker'>R$2500 </p>
+       </div>
+       <Button className='mt-3'>Reservar Agora</Button>
      </div>
    )
 }
--- src/app/trips/[tripId]/page.tsx
+++ src/app/trips/[tripId]/page.tsx
@@ -1,6 +1,8 @@
 import { prisma } from '@/lib/prisma'
 import React from 'react'
 import TripHeader from './components/TripHeader'
+import TripReservation from './components/TripReservation'
+import Button from '@/components/Button'
 
 const getTripsDetails = async (tripId: string) => {
   const trip = await prisma.trip.findUnique({
@@ -25,7 +27,8 @@ const TripDetails = async ({params}: {params: {tripId: string}}) => {
      <div className='container mx-auto'>
        <TripHeader trip={trip} />
 
-       {/* RESERVA */}
+       <TripReservation trip={trip}/>
+
      </div>
    )
 }
