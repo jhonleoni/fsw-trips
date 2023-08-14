f0dea7a5ac41e723e1753c5de360da1a513fb1c4
jhonleoni <jhonyalmeidax@gmail.com>
2023-08-13 22:58:34 -0400

    feat: add my trips button to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,51 +1,60 @@
-"use client"
+"use client";
 
-import React from "react"
-import Image from "next/image"
-import { signIn, signOut, useSession } from "next-auth/react"
-import { AiOutlineMenu } from "react-icons/ai"
-import Link from "next/link"
+import React from "react";
+import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { AiOutlineMenu } from "react-icons/ai";
+import Link from "next/link";
 
 const Header = () => {
-    const [menuIsOpen, setMenuIsOpen] = React.useState(false)
-    const { status, data } = useSession()
+  const [menuIsOpen, setMenuIsOpen] = React.useState(false);
 
-    const handleLoginClick = () => signIn()
-    const handleLogoutClick = () => {
-      setMenuIsOpen(false)
-      signOut()
-    }
+  const { status, data } = useSession();
 
-  return <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
-    <Link href="/">
-      <div className="relative h-[32px] w-[182px]">
-      <Image src="/logo.png" alt="full stack week" fill/>
-      </div>
-    </Link>
+  const handleLoginClick = () => signIn();
 
-    {status === "unauthenticated" && (
-      <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>
-      Login
-    </button>
-    )}
+  const handleLogoutClick = () => {
+    setMenuIsOpen(false);
+    signOut();
+  };
 
-    {status === "authenticated" && data.user && (
-      <div className="flex items-center gap-3 boder-lightGray border border-solid rounded-lg p-2 px-3 relative">
-        <AiOutlineMenu size={16} onClick={() => setMenuIsOpen(true)} className="cursor-pointer"/>
+  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
 
-        <Image height={35} width={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md"/>
-        {menuIsOpen && (
-          <div className="z-50 absolute top-12 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
-            <button className="text-primary text-xs font-semibold" onClick={handleLogoutClick}>
-              Logout
-            </button>
-          </div>
-        )
-        }
-      </div>
-    )
-    }
-  </div>
-} 
+  return (
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center lg:border-b lg:border-grayLighter">
+      <Link href="/">
+        <div className="relative h-[32px] w-[182px]">
+          <Image src="/logo.png" alt="Full Stack Week" fill />
+        </div>
+      </Link>
 
-export default Header
+      {status === "unauthenticated" && (
+        <button className="text-primary text-sm font-semibold" onClick={handleLoginClick}>
+          Login
+        </button>
+      )}
+
+      {status === "authenticated" && data.user && (
+        <div className="flex items-center gap-3 border-grayLighter border border-solid rounded-full p-2 px-3 relative">
+          <AiOutlineMenu size={16} onClick={handleMenuClick} className="cursor-pointer" />
+
+          <Image height={35} width={35} src={data.user.image!} alt={data.user.name!} className="rounded-full shadow-md" />
+
+          {menuIsOpen && (
+            <div className="z-50 absolute top-14 left-0 w-full h-[100px] bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+              <Link href="/my-trips" onClick={() => setMenuIsOpen(false)}>
+                <button className="text-primary pb-2 border-b border-grayLighter border-solid text-sm font-semibold">Minhas Viagens</button>
+              </Link>
+
+              <button className="text-primary pt-2 text-sm font-semibold" onClick={handleLogoutClick}>
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
